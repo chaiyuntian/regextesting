3d3176665f655610ffecd9b15a3f9fdf08ed690e
jackhuntcn <jack.hunt.cn@gmail.com>
2014-02-12 14:51:23 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+just a little tool written in python 2.7 and tkinter for regex testing
---
+++ regextesting.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+import re
+import sys
+if sys.getdefaultencoding() != 'utf-8':
+	reload(sys)
+	sys.setdefaultencoding('utf-8')
+from Tkinter import *
+import tkFont
+
+class my_decorator:
+	def __init__(self, function):
+		self.function = function
+	def __call__(self, *args):
+		try:
+			return self.function(*args)
+		except Exception, e:
+			res_text.insert(END, 'Error: %s\n' % e, 'red')
+
+def get_regex_and_strs():
+	return reg_entry.get(), str_text.get('1.0', END)
+
+def do_cleanup_res():
+	res_text.config(state=NORMAL)
+	res_text.delete('1.0', END)
+
+def do_cleanup_regex_and_strs():
+	reg_entry.delete('0', END)
+	str_text.delete('1.0', END)
+
+def do_cleanup():
+	do_cleanup_res()
+	do_cleanup_regex_and_strs()
+
+def do_reg():
+	if v.get() == 1: do_re_search()
+	if v.get() == 2: do_re_findall()
+	if v.get() == 3: do_re_sub()
+
+@my_decorator
+def do_re_search():
+	do_cleanup_res()
+	regex, strs = get_regex_and_strs()
+	try:
+		res = re.search(regex, strs)
+		if len(res.groups()) != 0:
+			i = 1
+			for item in res.groups():
+				res_text.insert(END, '*** group %i ***\n' % i, 'maroon')
+				i += 1
+				res_text.insert(END, item + '\n\n', 'green')
+		else:
+			res_text.insert(END, res.group(), 'green')
+	except:
+		res_text.insert(END, '*** NO MATCH ***', 'red')
+	res_text.config(state=DISABLED)
+
+@my_decorator
+def do_re_findall():
+	do_cleanup_res()
+	regex, strs = get_regex_and_strs()
+	res = re.findall(regex, strs)
+	if len(res) != 0:
+		i  = 0
+		for item in res:
+			res_text.insert(END, '*** findall list %i ***\n' % i, 'maroon')
+			i += 1
+			res_text.insert(END, item + '\n\n', 'green')
+	else:
+		res_text.insert(END, '*** NO MATCH ***', 'red')
+	res_text.config(state=DISABLED)
+
+@my_decorator
+def do_re_sub():
+	do_cleanup_res()
+	regex, strs = get_regex_and_strs()
+	rep_strs = rep_entry.get()
+	res = re.sub(regex, rep_strs, strs)
+	res_text.insert(END, res, 'green')
+	res_text.config(state=DISABLED)
+
+if __name__ == '__main__':
+
+	win = Tk()
+	win.wm_title(sys.argv[0])
+
+	myfont = tkFont.Font(family=u"文泉驿等宽微米黑", size=12, weight='normal')
+	
+	v = IntVar()
+
+	l1 = Label(win, text='regular expression', font=myfont, width=30)
+	l1.grid(row=0, column=0)
+	reg_entry = Entry(win, font=myfont, width=60, fg='green')
+	reg_entry.grid(row=1, column=0, columnspan=3, sticky=W+E+N+S)
+
+	l2 = Label(win, text='replace strings(re.sub *ONLY*)', font=myfont, width=30)
+	l2.grid(row=2, column=0)
+	rep_entry = Entry(win, font=myfont, width=60, fg='blue')
+	rep_entry.grid(row=3, column=0, columnspan=3, sticky=W+E+N+S)
+
+	l3 = Label(win, text='select a method', font=myfont, width=30)
+	l3.grid(row=4, column=0)
+	r1 = Radiobutton(win, text='re.search', font=myfont, variable=v, value=1, width=30)
+	r1.grid(row=5, column=0)
+	r2 = Radiobutton(win, text='re.findall', font=myfont, variable=v, value=2, width=30)
+	r2.grid(row=5, column=1)
+	r3 = Radiobutton(win, text='re.sub', font=myfont, variable=v, value=3, width=30)
+	r3.grid(row=5, column=2)
+	
+	l4 = Label(win, text='testing strings', font=myfont, width=30)
+	l4.grid(row=6, column=0)
+	str_text = Text(win, font=myfont, height=10, width=60)
+	str_text.grid(row=7, column=0, columnspan=3, sticky=W+E+N+S)
+	
+	l5 = Label(win, text='results', font= myfont, width=30)
+	l5.grid(row=8, column=0)
+	res_text = Text(win, font=myfont, height=10, width=60, state=DISABLED)
+	res_text.tag_config('green',  foreground='#008000')
+	res_text.tag_config('maroon', foreground='#800000')
+	res_text.tag_config('red',    foreground='#ff0000')
+	res_text.grid(row=9, column=0, columnspan=3, sticky=W+E+N+S)
+
+	DoButton = Button(win, text='Do', fg='green', font=myfont, width=30, command=do_reg)
+	DoButton.grid(row=10, column=0)
+	CleanButton = Button(win, text='Clean', fg='red', font=myfont, width=30, command=do_cleanup)
+	CleanButton.grid(row=10, column=1)
+	QuitButton = Button(win, text='Quit', fg='red', font=myfont, width=30, command=win.quit)
+	QuitButton.grid(row=10, column=2)
+	
+	mainloop()
